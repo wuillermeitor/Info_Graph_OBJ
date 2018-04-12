76dbf60c02ecafed082eeea99eb261864f09753b
Guillermo Sánchez <guillermosanchezsanchez@enti.cat>
2018-04-12 12:11:04 +0200

    hello commit

---
+++ glframework_starthere/code/load_obj.cpp
@@ -0,0 +1,16 @@
+//Include standard headers
+#include <stdio.h>
+#include<stdlib.h>
+#include<vector>
+
+#include<glm\gtc\type_ptr.hpp>
+#include<glm\gtc\matrix_transform.hpp>
+
+
+bool loadOBJ(	const char * path, 
+				std::vector<glm::vec3> & out_vertices, 
+				std::vector<glm::vec2> & out_uvs, 
+				std::vector<glm::vec3> & out_normals) 
+{
+	return true;
+}
---
+++ glframework_starthere/code/my_render_code.cpp
@@ -0,0 +1,11 @@
+
+#include <GL\glew.h>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+
+#include <cstdio>
+#include <cassert>
+
+#include "GL_framework.h"
+
+
